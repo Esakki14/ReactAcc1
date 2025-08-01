03e753dc2368719c5e1f9d6d69a0970b072a9623
Esakki14 <eesakki14@gmail.com>
2025-08-01 13:43:17 +0530

    sample

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css'
+export default function App() {
+  return (
+    <div>
+      <UserList />
+    </div>
+  );
+}
+
+function UserList() {
+  return (
+    <div>
+      <div className="App">
+        <User
+          hello="Hello"
+          name="Name1"
+          img="https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE="
+        />
+        <User
+          hello="Hello"
+          name="Name2"
+          img="https://imgv3.fotor.com/images/slider-image/A-clear-close-up-photo-of-a-woman.jpg"
+        />
+        <User
+          hello="Hello"
+          name="Name3"
+          img="https://media.gettyimages.com/id/468662696/photo/focusing-at-you.jpg?s=612x612&w=0&k=20&c=OOH7GJYckqk5JZEOAcuDI1_E4tCcG-QvSDhK8GyX2Rs="
+        />
+      </div>
+    </div>
+  );
+}
+
+function User({ name, hello, img }) {
+  return (
+    <>
+      <div className="userComp">
+        <img src={img} alt="img" />
+        <h3>
+          <span>{hello}, </span>
+          {name}
+        </h3>
+      </div>
+    </>
+  );
+}
